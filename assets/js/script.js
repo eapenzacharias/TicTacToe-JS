95c7f86f1b024a9cfc3afce43c660d3cca92a185
eapenzacharias <eapenzac@gmail.com>
2019-11-29 23:07:25 +0100

    Added create game

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,66 +12,113 @@ const game = (() => {
     const checkRow = () => {
         if (board[0] === board[1] && board[0] === board[2]) {
             return board[0];
-          } else if (board[3] === board[4] && board[3] === board[5]) {
+        } else if (board[3] === board[4] && board[3] === board[5]) {
             return board[3];
-          } else if (board[6] === board[7] && board[6] === board[8]) {
+        } else if (board[6] === board[7] && board[6] === board[8]) {
             return board[6];
-          } else {
+        } else {
             return 0;
-          }
+        }
     };
 
     const checkColumn = () => {
         if (board[0] === board[3] && board[0] === board[6]) {
-          return board[0];
+            return board[0];
         } else if (board[1] === board[4] && board[1] === board[7]) {
-          return board[1];
+            return board[1];
         } else if (board[2] === board[5] && board[2] === board[8]) {
-          return board[2];
+            return board[2];
         } else {
-          return 0;
+            return 0;
         }
-      };
+    };
 
-      const checkDiagonal = () => {
+    const checkDiagonal = () => {
         if (board[0] === board[4] && board[0] === board[8]) {
-          return board[0];
+            return board[0];
         } else if (board[2] === board[4] && board[2] === board[6]) {
-          return board[2];
+            return board[2];
         } else {
-          return 0;
+            return 0;
         }
-      };
+    };
 
-      const checkWinner = () => {
+    const checkWinner = () => {
         if (checkRow() !== 0 || checkColumn() !== 0 || checkDiagonal() !== 0) {
-          return checkRow() + checkColumn() + checkDiagonal();
+            return checkRow() + checkColumn() + checkDiagonal();
         } else if (!board.includes(0)) {
-          return 3;
+            return 3;
         } else {
-          return 0;
+            return 0;
         }
-      };
+    };
+
+    const move = (player, place) => {
+        if (player == 1) {
+            let cell = document.getElementById(place);
+            cell.innerHTML = "X";
+        } else {
+            let cell = document.getElementById(place);
+            cell.innerHTML = "O";
+        }
+        board[place] = player
+    }
 
     return {
         board: () => board,
-        move: (player, place) => board[place] = player,
-        checkRow,
-        checkColumn,
-        checkDiagonal,
+        move,
         checkWinner
     };
 })();
 
+function switchTurns(player) {
+    if (player == 1) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
 
+function gameStarter() {
+    let playerone = document.createElement("form");
+    document.getElementById("playerone").appendChild(playerone);
+    let PlayerOneName = document.createElement("input");
+    PlayerOneName.setAttribute("type", "text");
+    PlayerOneName.setAttribute("id", "playeronename");
+    document.getElementById("playerone").appendChild(PlayerOneName);
+    let playertwo = document.createElement("form");
+    document.getElementById("playertwo").appendChild(playertwo);
+    let PlayerTwoName = document.createElement("input");
+    PlayerTwoName.setAttribute("id", "playertwoname");
+    PlayerTwoName.setAttribute("type", "text");
+    document.getElementById("playertwo").appendChild(PlayerTwoName);
+    let startGame = document.createElement("button");
+    startGame.innerHTML("Start Game");
+    document.getElementById("player-details").appendChild(startGame);
+    startGame.onclick = () => RunnerFunction();
+}
 
+function RunnerFunction() {
+    let playerOneName = "Player 1";
+    let playerTwoName = "Plater 2";
+    if (document.getElementById("playeronename").value !== "") {
+        playerOneName = document.getElementById("playeronename").value;
+    }
+    if (document.getElementById("playertwoname").value !== "") {
+        playerTwoName = document.getElementById("playertwoname").value;
+    }
+    const player1 = player(playerOneName, 1);
+    const player2 = player(playerTwoName, 2);
 
-
+    game.board();
+    /*for (i = 0; i < 9; i++) {
+        let cell-`${i}` = document.getElementById(i);
+    }*/
+}
 
 
 //let boardDom = document.getElementById("board");
 
-console.log(game.checkRow());
 
 game.move(1, 0);
 game.move(1, 3);
