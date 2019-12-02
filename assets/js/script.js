b0fd6fe4cf62260a7783308bc2675715752976d8
eapenzacharias <eapenzac@gmail.com>
2019-12-02 22:35:21 +0100

    Game fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,11 @@ const game = (() => {
     let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     const checkRow = () => {
-        if (board[0] === board[1] && board[0] === board[2]) {
+        if (board[0] === board[1] && board[0] === board[2] && board[0] !== 0) {
             return board[0];
-        } else if (board[3] === board[4] && board[3] === board[5]) {
+        } else if (board[3] === board[4] && board[3] === board[5] && board[3] !== 0) {
             return board[3];
-        } else if (board[6] === board[7] && board[6] === board[8]) {
+        } else if (board[6] === board[7] && board[6] === board[8] && board[6] !== 0) {
             return board[6];
         } else {
             return 0;
@@ -22,11 +22,11 @@ const game = (() => {
     };
 
     const checkColumn = () => {
-        if (board[0] === board[3] && board[0] === board[6]) {
+        if (board[0] === board[3] && board[0] === board[6] && board[0] !== 0) {
             return board[0];
-        } else if (board[1] === board[4] && board[1] === board[7]) {
+        } else if (board[1] === board[4] && board[1] === board[7] && board[1] !== 0) {
             return board[1];
-        } else if (board[2] === board[5] && board[2] === board[8]) {
+        } else if (board[2] === board[5] && board[2] === board[8] && board[2] !== 0) {
             return board[2];
         } else {
             return 0;
@@ -34,9 +34,9 @@ const game = (() => {
     };
 
     const checkDiagonal = () => {
-        if (board[0] === board[4] && board[0] === board[8]) {
+        if (board[0] === board[4] && board[0] === board[8] && board[0] !== 0) {
             return board[0];
-        } else if (board[2] === board[4] && board[2] === board[6]) {
+        } else if (board[2] === board[4] && board[2] === board[6] && board[2] !== 0) {
             return board[2];
         } else {
             return 0;
@@ -66,8 +66,8 @@ const game = (() => {
 
     const clearBoard = () => {
         board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        let winner = document.getElementById("winner"); 
-        for (let i = 0; i < 9; i++){
+        let winner = document.getElementById("winner");
+        for (let i = 0; i < 9; i++) {
             let cell = document.getElementById(`${i}`);
             cell.innerHTML = '';
         }
@@ -94,77 +94,80 @@ function addCellEvent(currentPlayer) {
     for (let i = 0; i < 9; i++) {
         let cell = document.getElementById(`${i}`);
         cell.addEventListener("click", function() {
-                board = game.board();
-                if (board[i] == 0) {
-                    cell.removeEventListener("click", game.move(currentPlayer, i));
-                    if (game.checkWinner() == 0) {
-                        return currentPlayer = switchTurns(currentPlayer);
-                    } else {
-                        decide(game.checkWinner());
-                        //} else if (game.checkWinner() == 2) {
-                        //    console.log('player 2 wins!');
-                        //} else {
-                        //    console.log("DRAW");
-                        //}
-                    }
-                }});
-    
-}}
-    
+            board = game.board();
+            if (board[i] == 0) {
+                cell.removeEventListener("click", game.move(currentPlayer, i));
+                if (game.checkWinner() == 0) {
+                    return currentPlayer = switchTurns(currentPlayer);
+                } else {
+                    decide(game.checkWinner());
+                    //} else if (game.checkWinner() == 2) {
+                    //    console.log('player 2 wins!');
+                    //} else {
+                    //    console.log("DRAW");
+                    //}
+                }
+            }
+        });
 
-    function decide(i) {
-        let msgBoard = document.getElementById("winner");
-        let header = document.createElement("h3");
-        msgBoard.appendChild(header);
-        if (i == 1) {
-            var winnerText = document.createTextNode(`${player1.name} WINS!`);
-        } else if (i == 2) {
-            var winnerText = document.createTextNode(`${player2.name} WINS!`);
-        } else {
-            var winnerText = document.createTextNode(`DRAW`);
-        }
-        header.appendChild(winnerText);
-        //gameStarter();
     }
+}
 
-    function gameStarter() {
-        let playerone = document.createElement("form");
-        document.getElementById("playerone").appendChild(playerone);
-        let PlayerOneName = document.createElement("input");
-        PlayerOneName.setAttribute("type", "text");
-        PlayerOneName.setAttribute("id", "playeronename");
-        playerone.appendChild(PlayerOneName);
-        let playertwo = document.createElement("form");
-        document.getElementById("playertwo").appendChild(playertwo);
-        let PlayerTwoName = document.createElement("input");
-        PlayerTwoName.setAttribute("id", "playertwoname");
-        PlayerTwoName.setAttribute("type", "text");
-        playertwo.appendChild(PlayerTwoName);
-        let startGame = document.createElement("button");
-        startGame.onclick = () => RunnerFunction();
-        startGame.appendChild(document.createTextNode("Start Game"));
-        document.getElementById("player-details").appendChild(startGame);
+
+function decide(i) {
+    let msgBoard = document.getElementById("winner");
+    let header = document.createElement("h3");
+    msgBoard.appendChild(header);
+    if (i == 1) {
+        var winnerText = document.createTextNode(`${player1.name} WINS!`);
+    } else if (i == 2) {
+        var winnerText = document.createTextNode(`${player2.name} WINS!`);
+    } else {
+        var winnerText = document.createTextNode(`DRAW`);
     }
-    var player1;
-    var player2;
-    function RunnerFunction() {
-        game.clearBoard();
-        let playerOneName = "Player 1";
-        let playerTwoName = "Player 2";
-        if (document.getElementById("playeronename").value !== "") {
-            playerOneName = document.getElementById("playeronename").value;
-        }
-        if (document.getElementById("playertwoname").value !== "") {
-            playerTwoName = document.getElementById("playertwoname").value;
-        }
-         player1 = player(playerOneName, 1);
-         player2 = player(playerTwoName, 2);
-        let currentPlayer = 1;
-        game.board();
-        addCellEvent(currentPlayer);
+    header.appendChild(winnerText);
+    //gameStarter();
+}
+
+function gameStarter() {
+    let playerone = document.createElement("form");
+    document.getElementById("playerone").appendChild(playerone);
+    let PlayerOneName = document.createElement("input");
+    PlayerOneName.setAttribute("type", "text");
+    PlayerOneName.setAttribute("id", "playeronename");
+    playerone.appendChild(PlayerOneName);
+    let playertwo = document.createElement("form");
+    document.getElementById("playertwo").appendChild(playertwo);
+    let PlayerTwoName = document.createElement("input");
+    PlayerTwoName.setAttribute("id", "playertwoname");
+    PlayerTwoName.setAttribute("type", "text");
+    playertwo.appendChild(PlayerTwoName);
+    let startGame = document.createElement("button");
+    startGame.onclick = () => RunnerFunction();
+    startGame.appendChild(document.createTextNode("Start Game"));
+    document.getElementById("player-details").appendChild(startGame);
+}
+var player1;
+var player2;
+
+function RunnerFunction() {
+    game.clearBoard();
+    let playerOneName = "Player 1";
+    let playerTwoName = "Player 2";
+    if (document.getElementById("playeronename").value !== "") {
+        playerOneName = document.getElementById("playeronename").value;
     }
+    if (document.getElementById("playertwoname").value !== "") {
+        playerTwoName = document.getElementById("playertwoname").value;
+    }
+    player1 = player(playerOneName, 1);
+    player2 = player(playerTwoName, 2);
+    let currentPlayer = 1;
+    game.board();
+    addCellEvent(currentPlayer);
+}
 
 
-    //let boardDom = document.getElementById("board");
+//let boardDom = document.getElementById("board");
 
-    gameStarter();
+gameStarter();
